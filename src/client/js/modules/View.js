2a49dc80d58e57aa2b35c89e9d110b17bd4ee743
Frank Warnaar <f.warnaar@viriciti.com>
2017-04-21 15:14:52 +0200

    styling

--- src/client/js/modules/View.js
+++ src/client/js/modules/View.js
@@ -7,9 +7,10 @@ class View {
 		const $tableBody = document.querySelector('#ranking tbody');
 
 		let content = '';
-		users.forEach(user => {
+		users.forEach((user, i) => {
 			content += `
 				<tr>
+					<td>${i + 1}</td>
 					<td>${user.name}</td>
 					<td>${user.commits}</td>
 				</tr>`;
